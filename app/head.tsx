12255b91a7a0cf3fec2bb03824210b3602342132
Ahmer Arain <57433886+ahmerarain@users.noreply.github.com>
2023-09-24 00:59:24 +0500

    updated

--- app/head.tsx
+++ app/head.tsx
@@ -11,7 +11,7 @@ const Head = () => {
 
   const meta = {
     title: `Muhammad Ahmer ${pathname === '/' ? '' : '· ' + titleFixedToShow}`,
-    description: `Hi! My name is Muhammad Ahmer I'm from Pakistan and I have +2 year of work experience in JavaScript/TypeScript and React, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16. I love what I do and I help others with what I can, I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron implementing good practices, clean architecture, pixel perfect and agile methodologies.`,
+    description: `Hi! I'm Muhammad Ahmer from Pakistan. I am a Full Stack Developer with over 2 years of experience in JavaScript and TypeScript, I have a strong passion for web development and creating visually appealing user interfaces. I've been dedicated to continuous learning and exploring new technologies.`,
     keywords:
       'Muhammad Ahmer, ahmerarain, Muhammad Ahmer portfolio, ahmerarain portfolio, ahmerarain portfolio, Muhammad Ahmer portfolio, ahmerarain github, Muhammad Ahmer github, ahmerarain LinkedIn, Muhammad Ahmer LinkedIn, Web, Programming web,  Application web, frontend,backend HTML, CSS, Javascript, Typescript, React, Express, MERN, ',
     type: 'website'
